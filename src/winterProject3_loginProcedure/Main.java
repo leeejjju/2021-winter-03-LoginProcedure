ed29f72e36201f639ee58a7d8a212fc87b55bbaf
leeejjju <dlwlswn02027@gmail.com>
2021-12-28 22:13:37 +0900

    init project

---
+++ src/winterProject3_loginProcedure/Main.java
@@ -0,0 +1,16 @@
+package winterProject3_loginProcedure;
+
+public class Main {
+		/*
+	    *정보
+	    2021-12-28-18:00~ 2021-12-30-18:00
+	    2021-winter 과제3, 데이터베이스- 시스템 로그인 기능 만들기 
+	    HGU 전산전자공학부_ 22100579 이진주
+	    */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!!!");
+		//test
+	}
+
+}
